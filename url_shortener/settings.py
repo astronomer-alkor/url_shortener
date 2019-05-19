35ad814c1db52836185948bdd6d1d90af9c644d1
Korotynski Alexey <korotynski.alexey@gmail.com>
2019-05-20 00:34:57 +0300

    Add russian locale

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wc*k+i@-x1h-w1r#(l$n#60_-o4)z+wcg#kea860&@+gjqm(sq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
